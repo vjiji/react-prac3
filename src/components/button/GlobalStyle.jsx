bd833b445d4bf927bee92eb9fdfc7500b4237711
kihyun <kim724265@gmail.com>
2024-03-01 10:12:17 +0900

    second commit Button fin

---
+++ src/components/button/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+import reset from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+  --color-mint: #55efc4;
+  --color-coral: #fab1a0;
+  --color-red: #d63031;
+  }
+`
+
+export default GlobalStyle;
