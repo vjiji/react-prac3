f64112e8c444b010f00331236ea61033a97c233d
vjiji <restmi0711@gmail.com>
2024-03-01 19:10:38 +0900

    feat: Modals에서 버튼 클릭 시 모달이 렌더링되도록 한다

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,5 +1,41 @@
-const Modal = () => {
-  return <div>Modal</div>;
+import styled from "styled-components";
+
+interface ModalProps {
+  isOpen: boolean;
+  setModalClose: () => void;
+}
+
+const Modal = ({ isOpen, setModalClose }: ModalProps) => {
+  if (!isOpen) {
+    return null;
+  }
+  console.log(isOpen);
+  return (
+    <ModalBackground>
+      <ModalStyles>
+        <button onClick={setModalClose}>닫기</button>
+      </ModalStyles>
+    </ModalBackground>
+  );
 };
 
 export default Modal;
+
+const ModalBackground = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: #000;
+  opacity: 0.5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalStyles = styled.div`
+  width: 50%;
+  height: 50%;
+  background-color: #fff;
+`;
--- src/components/features/Modals/Modals.tsx
+++ src/components/features/Modals/Modals.tsx
@@ -1,12 +1,48 @@
+import { Button } from "components/button/Button";
 import Modal from "components/common/Modal";
+import { useState } from "react";
+import styled from "styled-components";
 
 const Modals = () => {
+  const [isFirstModalOpen, setIsFirstModalOpen] = useState<boolean>(false);
+
+  const [isSecondModalOpen, setIsSecondModalOpen] = useState<boolean>(false);
+
   return (
     <>
-      <Modal />
-      <Modal />
+      <ButtonBox>
+        <Button
+          onClickFnc={() => setIsFirstModalOpen(!isFirstModalOpen)}
+          type="small"
+          theme="mint"
+        >
+          open modal
+        </Button>
+        <Button
+          onClickFnc={() => setIsFirstModalOpen(!isFirstModalOpen)}
+          type="large"
+          stroke="false"
+          bold="true"
+          fontco="red"
+        >
+          open modal
+        </Button>
+      </ButtonBox>
+      <Modal
+        isOpen={isFirstModalOpen}
+        setModalClose={() => setIsFirstModalOpen(!isFirstModalOpen)}
+      />
+      <Modal
+        isOpen={isSecondModalOpen}
+        setModalClose={() => setIsSecondModalOpen(!isSecondModalOpen)}
+      />
     </>
   );
 };
 
 export default Modals;
+
+const ButtonBox = styled.div`
+  display: flex;
+  gap: 10px;
+`;
